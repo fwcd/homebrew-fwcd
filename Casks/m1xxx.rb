d27201d3bf117aa62496a14b5a8c8e7b96f9cb2b
fwcd <fwcdmail@gmail.com>
2022-12-23 02:46:36 +0100

    Use manual bump-cask-pr to bump version

--- Casks/m1xxx.rb
+++ Casks/m1xxx.rb
@@ -2,7 +2,7 @@ cask "m1xxx" do
   version "2.4.0-0-929fa9d"
   sha256 "a712510ffb31b2db614a74edd3e93630603fee49ade367f53564e274ff75f5b7"
 
-  url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{version.split("-").last}.dmg"
+  url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{version}.dmg"
   name "M1xxx"
   desc "Unofficial Mixxx build for Apple Silicon"
   homepage "https://github.com/fwcd/m1xxx"
