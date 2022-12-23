3e2334a787e3f5c0ffabc6d7f7835ca7cf53c4d1
fwcd <fwcdmail@gmail.com>
2022-12-23 03:09:56 +0100

    Manually bump m1xxx to latest

--- Casks/m1xxx.rb
+++ Casks/m1xxx.rb
@@ -1,6 +1,6 @@
 cask "m1xxx" do
-  version "2.4.0-0-929fa9d"
-  sha256 "a712510ffb31b2db614a74edd3e93630603fee49ade367f53564e274ff75f5b7"
+  version "2.4.0.c0.r0a15ae8951"
+  sha256 "f2c19c7e15e624a227bf1fbc1977b65fbf6a96d5fddc47ef90849f184f472b7a"
 
   url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{version}.dmg"
   name "M1xxx"
