4715a5342d86ccac11538bdec044e32b10feac5f
fwcd <fwcdmail@gmail.com>
2022-12-23 01:14:48 +0100

    Fix style issues in m1xxx cask

--- Casks/m1xxx.rb
+++ Casks/m1xxx.rb
@@ -2,17 +2,18 @@ cask "m1xxx" do
   version "2.4.0-0-929fa9d"
   sha256 "a712510ffb31b2db614a74edd3e93630603fee49ade367f53564e274ff75f5b7"
 
-  url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{version.split('-').last}.dmg"
+  url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{version.split("-").last}.dmg"
   name "M1xxx"
   desc "Unofficial Mixxx build for Apple Silicon (arm64 macOS)"
   homepage "https://github.com/fwcd/m1xxx"
 
   livecheck do
-    url 'https://github.com/fwcd/m1xxx/releases/latest'
-    regex /tag\/v([\w\.\-]+)/
+    url "https://github.com/fwcd/m1xxx/releases/latest"
+    regex %r{tag/v([\w.-]+)}
     strategy :header_match do |headers, regex|
-      next if headers['location'].blank?
-      headers['location'].scan(regex).map { |match| match[0] }
+      next if headers["location"].blank?
+
+      headers["location"].scan(regex).map { |match| match[0] }
     end
   end
 
