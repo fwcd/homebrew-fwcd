c6fac8a6df3d51d7720a5a85f45d8a2dcee57a1f
fwcd <fwcdmail@gmail.com>
2023-11-12 12:07:41 +0000

    Update `m1xxx` for new x86_64/arm64 naming scheme
    Fixes #5

--- Casks/m1xxx.rb
+++ Casks/m1xxx.rb
@@ -1,10 +1,14 @@
 cask "m1xxx" do
-  arch arm: "arm64"
-  
-  version "2.5.0.c45818.r30bca40dad"
-  sha256 arm: "52bd2cb34c7a766fde17694b95bf1623c7f66c4cc7bc2e60d0a43ea44303b2e2"
+  arch arm:   "arm64",
+       intel: "x64"
+  deployment_target = on_arch_conditional arm:   "min1100",
+                                          intel: "min1015"
 
-  url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{arch}-osx-#{version}.dmg"
+  version "2.5.0.c45818.r30bca40dad"
+  sha256 arm:   "a98a275b80c17cd4ba1b1ecc1610b5fb260c7928f37280fff4844919076759e6",
+         intel: "c4078335ceb2f4e98c6c9b8103b41cbc5690fa4055eea465b7c7763a9a12ab58"
+
+  url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{arch}-osx-#{deployment_target}-#{version}.dmg"
   name "M1xxx"
   desc "Unofficial build of the Free and Open Source DJ software Mixxx"
   homepage "https://github.com/fwcd/m1xxx"
