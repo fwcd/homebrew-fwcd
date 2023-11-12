55314d7093a7a6d3bdc2b806ad291373fc35ddca
fwcd <30873659+fwcd@users.noreply.github.com>
2023-11-12 10:14:22 +0000

    Update m1xxx path

--- Casks/m1xxx.rb
+++ Casks/m1xxx.rb
@@ -1,10 +1,12 @@
 cask "m1xxx" do
+  arch arm: "arm64"
+  
   version "2.5.0.c45818.r30bca40dad"
-  sha256 "52bd2cb34c7a766fde17694b95bf1623c7f66c4cc7bc2e60d0a43ea44303b2e2"
+  sha256 arm: "52bd2cb34c7a766fde17694b95bf1623c7f66c4cc7bc2e60d0a43ea44303b2e2"
 
-  url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{version}.dmg"
+  url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{arch}-osx-#{version}.dmg"
   name "M1xxx"
-  desc "Unofficial Mixxx build for Apple Silicon"
+  desc "Unofficial build of the Free and Open Source DJ software Mixxx"
   homepage "https://github.com/fwcd/m1xxx"
 
   livecheck do
