746fc008fd9a79689bcaf9c285aa8938d8b20f35
fwcd <fwcdmail@gmail.com>
2023-11-23 23:17:26 +0000

    Update `m1xxx` cask url

--- Casks/m1xxx.rb
+++ Casks/m1xxx.rb
@@ -8,7 +8,7 @@ cask "m1xxx" do
   sha256 arm:   "d045baa3ecdd17c43afde844f5ad62206d69e3470f6397c697b7dd8bad689073",
          intel: "4f92256f22cd5ec39e206fd4378170c57541221d8d16fc30317dbfdfce35e4e9"
 
-  url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{arch}-osx-#{deployment_target}-#{version}.dmg"
+  url "https://github.com/fwcd/m1xxx/releases/download/v#{version}/mixxx-#{version}-#{arch}-osx-#{deployment_target}.dmg"
   name "M1xxx"
   desc "Unofficial build of the Free and Open Source DJ software Mixxx"
   homepage "https://github.com/fwcd/m1xxx"
